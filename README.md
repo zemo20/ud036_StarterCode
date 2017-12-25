1fddc1bb500a7f268dd3631db7a34441ba71c3b8
zemo20 <zem020@hotmail.com>
2017-12-26 00:52:07 +0200

    updating comments and readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# project
+# Project
 A website that shows movies with their trailers,a udacity python tutorial project.
 
 # APIs used
 -TMDB api to search for movie details.
+
+#How to run
+-install python 3.6.
+-git clone https://github.com/zemo20/ud036_StarterCode.git
+-run idle.
+-browse the cloned project and run entertainment_center.py.
--- media.py
+++ media.py
@@ -1,8 +1,10 @@
 import webbrowser
 class Movie():
+    #the paths for youtube videos, and images on tmdb website.
     TMDB_IMAGE_PATH = 'https://image.tmdb.org/t/p/w640'
     YOUTUBE_VIDEO_PATH = 'https://www.youtube.com/watch?v='
-    
+
+    #construct the movie with the needed data.
     def __init__(self, movie_details, movie_videos):
         self.title = movie_details['title']
         
@@ -11,7 +13,8 @@ class Movie():
         self.poster_image_url = self.TMDB_IMAGE_PATH + movie_details['poster_path']
    
         self.trailer_youtube_url = self.YOUTUBE_VIDEO_PATH + movie_videos['results'][0]['key']
-        
+
+    #open a webpage with the data taken from the api.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
