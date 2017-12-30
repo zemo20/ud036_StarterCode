6b62931d026cb5e849a10425c416e29d88e011ed
zemo20 <zem020@hotmail.com>
2017-12-30 22:45:59 +0200

    following pep8 guidelines

--- media.py
+++ media.py
@@ -1,25 +1,20 @@
 import webbrowser
+
+
 class Movie():
-    #the paths for youtube videos, and images on tmdb website.
+    # the paths for youtube videos, and images on tmdb website.
     TMDB_IMAGE_PATH = 'https://image.tmdb.org/t/p/w640'
     YOUTUBE_VIDEO_PATH = 'https://www.youtube.com/watch?v='
 
-    #construct the movie with the needed data.
+    # construct the movie with the needed data.
     def __init__(self, movie_details, movie_videos):
         self.title = movie_details['title']
-        
         self.storyline = movie_details['overview']
-        
-        self.poster_image_url = self.TMDB_IMAGE_PATH + movie_details['poster_path']
-   
-        self.trailer_youtube_url = self.YOUTUBE_VIDEO_PATH + movie_videos['results'][0]['key']
+        self.poster_image_url = (self.TMDB_IMAGE_PATH +
+                                 movie_details['poster_path'])
+        self.trailer_youtube_url = (self.YOUTUBE_VIDEO_PATH +
+                                    movie_videos['results'][0]['key'])
 
-    #open a webpage with the data taken from the api.
+    # open a webpage with the data taken from the api.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-
-    
-
-    
-        
